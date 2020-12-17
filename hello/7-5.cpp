d78324e125b0b699911f33a49f72238aea3792e3
N.Fujita <naofuji.1220@gmail.com>
2020-12-18 01:11:32 +0900

    inital commit

---
+++ hello/7-5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+int num;       // 入力した数
+int i;         // インクリメント
+
+
+int main(){
+    std::cout << "任意の数を入力してください" << "\n";
+    std::cin >> num;
+    
+    i = 1;
+    while (i < num - 1) {
+        ++i;
+        if (num % i == 0) {
+            std::cout << "入力した値は素数ではありません" << "\n";
+            break;
+        }
+        if (i == num - 1) {
+            std::cout << "入力した値は素数です" << "\n";
+        }
+    }
+    return 0;
+}
+
+            
+
+
+
+
+
